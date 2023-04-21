18a5b1f9e225802f15163a8218c7c30fd3dee74a
Artus U <artusrock@hotmail.com>
2023-04-21 10:29:51 +0100

    Login axios Fixes #14

--- eduflow_vue/src/views/LogIn.vue
+++ eduflow_vue/src/views/LogIn.vue
@@ -14,18 +14,25 @@
                         <div class="field">
                             <label>Email</label>
                             <div class="control">
-                                <input type="email" class="input">
+                                <input type="email" class="input" v-model="username">
                             </div>
                         </div>
 
                         <div class="field">
                             <label>Password</label>
                             <div class="control">
-                                <input type="password" class="input">
+                                <input type="password" class="input" v-model="password">
                             </div>
                         </div>
 
-                        <div class="notification is-danger">
+                        <div class="notification is-danger" v-if="errors.length">
+                            <p
+                                v-for="error in errors"
+                                v-bind:key="error"
+                            >
+                                {{ error }}
+                            </p>
+                        </div>
                             
                         </div>
 
@@ -47,5 +54,59 @@
 </template>
 
 <script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            errors: []
+        }
+    },
+    mounted() {
+        document.title = 'Log in | StudyNet'
+    },
+    methods: {
+        submitForm() {
+            console.log('submitForm')
+            axios.defaults.headers.common['Authorization'] = ""
+            localStorage.removeItem('token')
+            this.errors = []
+            if (this.username === '') {
+                this.errors.push('The username is missing!')
+            }
+            if (this.password === '') {
+                this.errors.push('The password is missing!')
+            }
+            if (!this.errors.length) {
+                const formData = {
+                    username: this.username,
+                    password: this.password
+                }
+                axios
+                    .post('token/login/', formData)
+                    .then(response => {
+                        const token = response.data.auth_token
+                        this.$store.commit('setToken', token)
+                        axios.defaults.headers.common['Authorization'] = "Token " + token
+                        localStorage.setItem('token', token)
+                        this.$router.push('/dashboard/my-account')
+                    })
+                    .catch(error => {
+                        if (error.response) {
+                            for (const property in error.response.data) {
+                                this.errors.push(`${property}: ${error.response.data[property]}`)
+                            }
+                            console.log(JSON.stringify(error.response.data))
+                        } else if (error.message) {
+                            this.errors.push('Something went wrong. Please try again')
+                            
+                            console.log(JSON.stringify(error))
+                        }
+                    })
+            }
+        }
+    }
+}
 
 </script>
