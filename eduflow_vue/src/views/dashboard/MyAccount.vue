35893fcff75f4f40826f95aad69ee164158e623b
Artus U <artusrock@hotmail.com>
2023-04-26 10:49:28 +0100

    LogOut functionality Fixes #18

--- eduflow_vue/src/views/dashboard/MyAccount.vue
+++ eduflow_vue/src/views/dashboard/MyAccount.vue
@@ -14,8 +14,24 @@
             CourseItem
           </div>
         </div>
-        <button class="button is-danger">Log out</button>
+        <button @click="logout()" class="button is-danger">Log out</button>
       </section>
     </div>
   </template>
   
+
+<script>
+import axios from 'axios'
+export default {
+  methods: {
+    logout() {
+      console.log("logout")
+
+      axios.defaults.headers.common['Authorization'] = ""
+      localStorage.removeItem('token')
+      this.$store.commit('removeToken')
+      this.$router.push('/')
+    }
+  }
+}
+</script>
