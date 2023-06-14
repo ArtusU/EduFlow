248f37b6cbc353f4b4bc5c4fb33981e48bb673c4
Artus U <artusrock@hotmail.com>
2023-06-14 10:02:34 +0100

    filtering categories

--- course/urls.py
+++ course/urls.py
@@ -5,6 +5,7 @@ from course import views
 urlpatterns = [
     path("", views.get_courses),
     path("get_frontpage_courses/", views.get_frontpage_courses),
+    path("get_categories/", views.get_categories),
     path("<slug:slug>/", views.get_course),
     path("get_author_courses/<int:user_id>/", views.get_author_courses),
     path("<slug:course_slug>/<slug:lesson_slug>/", views.add_comment),
--- eduflow_vue/src/views/Courses.vue
+++ eduflow_vue/src/views/Courses.vue
@@ -13,10 +13,21 @@
                         <aside class="menu">
                             <p class="menu-label">Categories</p>
                             <ul class="menu-list">
-                                <li><a href="">All Courses</a></li>
-                                <li><a href="">Programing</a></li>
-                                <li><a href="">Design</a></li>
-                                <li><a href="">UX</a></li>
+                                <li>
+                                    <a 
+                                        v-bind:class="{'is-active': !activeCategory}"
+                                        @click="setActiveCategory(null)"
+                                    >
+                                        All courses
+                                    </a>
+                                </li>
+
+                                <li v-for="category in categories"
+                                    v-bind:key="category.id"
+                                    @click="setActiveCategory(category)"
+                                    >
+                                    <a>{{ category.title }}</a>
+                                </li>
                             </ul>
                         </aside>
                     </div>
@@ -55,25 +66,50 @@ export default {
     data() {
         return {
             courses: [],
+            categories: [],
+            activeCategory: null
         }
     },
     components: {
         CourseItem
     },
-    mounted() {
+    async mounted() {
 
         document.title = 'Courses | EduFlow'
 
-        axios
-            .get('/courses/')
+        await axios
+            .get('/courses/get_categories/')
             .then(response => {
-                console.log(response.data)
+                // console.log(response.data)
 
-                this.courses = response.data
-
+                this.categories = response.data
             })
-        
+        this.getCourses()
     },
+    methods: {
+        setActiveCategory(category) {
+            // console.log(category)
+            this.activeCategory = category
+
+            this.getCourses()
+        },
+        getCourses() {
+            let url = 'courses/'
+
+            if (this.activeCategory) {
+                url += '?category_id=' + this.activeCategory.id
+            }
+
+            axios
+                .get(url)
+                .then(response => {
+                    // console.log(response.data)
+
+                    this.courses = response.data
+                })
+        }
+    }
+    
 
 }
 </script>
