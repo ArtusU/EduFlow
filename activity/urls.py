3cf4c3be86be2e18debdf12f60fb20897c0851be
Artus U <artusrock@hotmail.com>
2023-06-28 10:00:24 +0100

    passing activitie to the frontend

--- activity/urls.py
+++ activity/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_active_courses/', views.get_active_courses),
+    path("get_active_courses/", views.get_active_courses),
 ]
