7a1db189bce0ddaaa24afd62a3c3291fc289435b
Artus U <artusrock@hotmail.com>
2023-07-04 10:05:39 +0100

    getCategories

--- eduflow_vue/src/views/dashboard/CreateCourse.vue
+++ eduflow_vue/src/views/dashboard/CreateCourse.vue
@@ -12,24 +12,28 @@
 
                 <div class="field">
                     <label>Title</label>
-                    <input type="text" class="input">
+                    <input type="text" class="input" v-model="form.title">
                 </div>
 
                 <div class="field">
                     <label>Short description</label>
-                    <textarea class="textarea"></textarea>
+                    <textarea class="textarea" v-model="form.short_description"></textarea>
                 </div>
 
                 <div class="field">
                     <label>Long description</label>
-                    <textarea class="textarea"></textarea>
+                    <textarea class="textarea" v-model="form.long_description"></textarea>
                 </div>
 
                 <div class="field">
                     <div class="select is-multiple">
-                        <select multiple size="10">
-                            <option>
-                                Option 1
+                        <select multiple size="10" v-model="form.categories">
+                            <option 
+                                v-for="category in categories"
+                                v-bind:key="category.id"
+                                v-bind:value="category.id"
+                            >
+                                {{ category.title }}
                             </option>
                         </select>
                     </div>
@@ -39,7 +43,11 @@
             <div class="mb-6 px-6 py-4 has-background-grey-lighter">
                 <h2 class="subtitle">Lessons</h2>
 
-                <div class="mb-4">
+                <div
+                    v-for="(lesson, index) in form.lessons"
+                    v-bind:key="index"
+                    class="mb-4"
+                >
                     <h3 class="subtitle is-size-6">Lesson</h3>
 
                     <div class="field">
@@ -47,29 +55,67 @@
                         <input 
                             type="text" 
                             class="input" 
+                            v-model="lesson.title"
+                            :name="`form.lessons[${index}][title]`"
                         >
                     </div>
 
                     <div class="field">
                         <label>Short description</label>
-                        <textarea class="textarea"></textarea>
+                        <textarea class="textarea" v-model="lesson.short_description" :name="`form.lessons[${index}][short_description]`"></textarea>
                     </div>
 
                     <div class="field">
                         <label>Long description</label>
-                        <textarea class="textarea"></textarea>
+                        <textarea class="textarea" v-model="lesson.long_description" :name="`form.lessons[${index}][long_description]`"></textarea>
                     </div>
 
                     <hr>
                 </div>
 
-                <button class="button is-primary">Add lesson</button>
+                <button class="button is-primary" @click="addLesson()">Add lesson</button>
             </div>
 
             <div class="field buttons">
-                <button class="button is-success">Save as draft</button>
-                <button class="button is-info">Submit for review</button>
+                <button class="button is-success" @click="submitForm('draft')">Save as draft</button>
+                <button class="button is-info" @click="submitForm('review')">Submit for review</button>
             </div>
         </section>
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            form: {
+                title: '',
+                short_description: '',
+                long_description: '',
+                categories: [],
+                status: '',
+                lessons: []
+            },
+            categories: [],
+        }
+    },
+    mounted() {
+        this.getCategories()
+    },
+    methods: {
+        getCategories() {
+            console.log('getCategories')
+
+            axios
+                .get('courses/get_categories/')
+                .then(response => {
+                    console.log(response.data)
+
+                    this.categories = response.data
+                })
+        },
+    }
+}
+</script>
